05e5af418b2b950a91c4a882b8ca21a311bd9df6
sinmetal <sinmetal@sinmetalcraft.jp>
2026-08-02 12:18:54 +0900

    Apply PR #110's Read index doc comment fix to the v2 legacy index module
    refs
    
    * https://github.com/cloudspannerecosystem/yo/pull/205

--- v2/test/testmodels/legacy_default/snake_case.yo.go
+++ v2/test/testmodels/legacy_default/snake_case.yo.go
@@ -231,7 +231,7 @@ func FindSnakeCasesByStringIDFooBarBaz(ctx context.Context, db YODB, stringID st
 // used for primary key, index key and storing columns. If you need more columns, add storing
 // columns or Read by primary key or Query with join.
 //
-// Generated from unique index 'snake_cases_by_string_id'.
+// Generated from index 'snake_cases_by_string_id'.
 func ReadSnakeCasesByStringIDFooBarBaz(ctx context.Context, db YODB, keys spanner.KeySet) ([]*SnakeCase, error) {
 	var res []*SnakeCase
 	columns := []string{
